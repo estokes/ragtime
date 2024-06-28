f70bd6f4644a1246125d9ba6a4dd92bff8c8738f
Eric Stokes <eestokes@pm.me>
2024-06-28 16:38:06 -0400

    generalizing backends

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-use crate::{db_llama::EmbedDb, doc::DocStore, qa_llama::QaModel};
+use crate::doc::{ChunkId, DocStore};
 use anyhow::{anyhow, bail, Result};
-use llama_cpp_2::llama_backend::LlamaBackend;
+use compact_str::CompactString;
 use ort::Session;
-use std::{cmp::min, fs, path::Path, thread::available_parallelism, sync::Arc};
+use std::{cmp::min, fs, path::Path, sync::Arc, thread::available_parallelism};
 use tokenizers::Tokenizer;
+use usearch::ffi::Matches;
 
 pub mod doc;
-pub mod db;
-pub mod db_llama;
-pub mod qa_llama;
-pub mod qa_onnx;
+pub mod llama;
+pub mod onnx;
 
 fn session_from_model_file<P: AsRef<Path>>(model: P, tokenizer: P) -> Result<(Session, Tokenizer)> {
     let session = Session::builder()?
@@ -20,25 +19,52 @@ fn session_from_model_file<P: AsRef<Path>>(model: P, tokenizer: P) -> Result<(Se
     Ok((session, tokenizer))
 }
 
-pub struct RagQa {
-    docs: DocStore,
-    db: EmbedDb,
-    qa: QaModel,
+pub trait Persistable: Sized {
+    type Ctx;
+
+    fn save<P: AsRef<Path>>(&self, path: P) -> Result<()>;
+    fn load<P: AsRef<Path>>(ctx: Self::Ctx, path: P, view: bool) -> Result<Self>;
 }
 
-impl RagQa {
-    pub fn new<P: AsRef<Path>>(
-        backend: Arc<LlamaBackend>,
-        max_mapped: usize,
-        embed_model: P,
-        qa_model: P,
-    ) -> Result<Self> {
-        let docs = DocStore::new(max_mapped);
-        let db = EmbedDb::new(backend.clone(), embed_model)?;
-        let qa = QaModel::new(backend, qa_model)?;
-        Ok(Self { docs, db, qa })
-    }
+pub trait EmbedModel: Sized {
+    type Args;
 
+    fn new(args: Self::Args) -> Result<Self>;
+    fn add(&mut self, text: &[(ChunkId, &str)]) -> Result<()>;
+    fn search(&mut self, q: &str, n: usize) -> Result<Matches>;
+}
+
+pub trait QaPrompt {
+    fn with_capacity(n: usize) -> Self;
+    fn system<'a>(&'a mut self) -> impl std::fmt::Write + 'a;
+    fn user<'a>(&'a mut self) -> impl std::fmt::Write + 'a;
+    fn finalize(&mut self);
+    fn clear(&mut self);
+}
+
+pub trait QaModel: Sized {
+    type Args;
+    type Prompt: QaPrompt;
+
+    fn new(args: Self::Args) -> Result<Self>;
+    fn ask<'a>(
+        &'a mut self,
+        q: Self::Prompt,
+        gen_max: Option<usize>,
+    ) -> Result<impl Iterator<Item = Result<CompactString>> + 'a>;
+}
+
+pub struct RagQa<E, Q> {
+    docs: DocStore,
+    db: E,
+    qa: Q,
+}
+
+impl<E, Q> RagQa<E, Q>
+where
+    E: Persistable,
+    Q: Persistable,
+{
     /// save the state to the specified directory, which will be
     /// created if it does not exist.
     pub fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
@@ -57,14 +83,36 @@ impl RagQa {
     }
 
     /// load the state from the specified directory
-    pub fn load<P: AsRef<Path>>(backend: Arc<LlamaBackend>, path: P, view: bool) -> Result<Self> {
+    pub fn load<P: AsRef<Path>>(
+        embed_ctx: E::Ctx,
+        qa_ctx: Q::Ctx,
+        path: P,
+        view: bool,
+    ) -> Result<Self> {
         let path = path.as_ref();
         if !path.is_dir() {
             bail!("save directory could not be found")
         }
         let docs = DocStore::load(path.join("docs.json"))?;
-        let db = EmbedDb::load(backend.clone(), path.join("db.json"), view)?;
-        let qa = QaModel::load(backend, path.join("model.json"))?;
+        let db = E::load(embed_ctx, path.join("db.json"), view)?;
+        let qa = Q::load(qa_ctx, path.join("model.json"), view)?;
+        Ok(Self { docs, db, qa })
+    }
+}
+
+impl<E, Q> RagQa<E, Q>
+where
+    E: EmbedModel,
+    Q: QaModel,
+{
+    pub fn new<P: AsRef<Path>>(
+        max_mapped: usize,
+        embed_args: E::Args,
+        qa_args: Q::Args,
+    ) -> Result<Self> {
+        let docs = DocStore::new(max_mapped);
+        let db = E::new(embed_args)?;
+        let qa = Q::new(qa_args)?;
         Ok(Self { docs, db, qa })
     }
 
@@ -84,55 +132,36 @@ impl RagQa {
         Ok(())
     }
 
-    pub fn encode_prompt<S: AsRef<str>>(
-        &mut self,
-        context: &mut String,
-        rag: Option<S>,
-        q: S,
-    ) -> Result<String> {
+    fn encode_prompt(&mut self, q: &str) -> Result<Q::Prompt> {
         use std::fmt::Write;
-        let q = q.as_ref();
-        let mut prompt = String::with_capacity(min(4 * 1024 * 1024, q.len() * 10));
-//        write!(prompt, "<|system|>RAG is available if you have questions. If you want to search using RAG, say <RAG>your question</RAG> End your text there, and the system will search the database for your question. The answer to your question will be given using additional <|system|>prompts. Only answer the user's question once you have all the information that you need, you can make multiple <RAG> queries to collect the required information. Don't surround the final answer to the user with <RAG></RAG> otherwise the system will interpret it as an additional RAG query. <|end|>")?;
-        write!(prompt, "{context}\n")?;
-        if let Some(rag) = rag {
-            let rag = rag.as_ref();
-            let matches = self.db.search(rag, 3)?;
-            for dst in [&mut prompt, context] {
-                if matches.distances.len() == 0 || matches.distances[0] > 0.7 {
-                    write!(dst, "<|system|>There was no relevant information available about \"{rag}\" in the database<|end|>\n")?;
-                } else {
-                    write!(dst, "<|system|>")?;
-                    for (id, dist) in matches.keys.iter().zip(matches.distances.iter()) {
-                        if *dist <= 0.7 {
-                            let chunk = self.docs.get_chunk(*id)?;
-                            let s = self.docs.get(&chunk)?;
-                            write!(dst, "{s}\n\n")?;
-                        }
-                    }
-                    write!(dst, " <|end|>\n")?;
-                };
-            }
-        }
-        write!(prompt, "<|user|>{q} <|end|><|assistant|>")?;
-        Ok(prompt)
-    }
-
-    pub fn ask<S: AsRef<str>>(&mut self, q: S, gen_max: usize) -> Result<String> {
-        let mut context = String::new();
-        let mut prompt = self.encode_prompt(&mut context, Some(q.as_ref()), q.as_ref())?;
-        loop {
-            let res = self.qa.ask(&prompt, gen_max)?;
-            match res
-                .trim()
-                .strip_prefix("<RAG>")
-                .and_then(|s| s.strip_suffix("</RAG>"))
-            {
-                None => break Ok(res),
-                Some(rag) => {
-                    prompt = self.encode_prompt(&mut context, Some(rag), q.as_ref())?;
+        let mut prompt = Q::Prompt::with_capacity(min(4 * 1024 * 1024, q.len() * 10));
+        let matches = self.db.search(q, 3)?;
+        let mut dst = prompt.system();
+        if matches.distances.len() == 0 || matches.distances[0] > 0.7 {
+            write!(
+                dst,
+                "There was no relevant information available about \"{q}\" in the database\n"
+            )?;
+        } else {
+            for (id, dist) in matches.keys.iter().zip(matches.distances.iter()) {
+                if *dist <= 0.7 {
+                    let chunk = self.docs.get_chunk(*id)?;
+                    let s = self.docs.get(&chunk)?;
+                    write!(dst, "{s}\n\n")?;
                 }
             }
         }
+        write!(prompt.user(), "{q}")?;
+        prompt.finalize();
+        Ok(prompt)
+    }
+
+    pub fn ask<'a, S: AsRef<str>>(
+        &'a mut self,
+        q: S,
+        gen_max: Option<usize>,
+    ) -> Result<impl Iterator<Item = Result<CompactString>> + 'a> {
+        let prompt = self.encode_prompt(q.as_ref())?;
+        self.qa.ask(prompt, gen_max)
     }
 }
---
+++ src/onnx/mod.rs
@@ -0,0 +1,2 @@
+pub mod qa;
+pub mod db;
